ed647a5d6ff0241e495bba99655f357306aa73af
ijlee2 <ijlee2@users.noreply.github.com>
2022-07-25 14:40:59 +0200

    feature: Updated the migration strategy for route stylesheets

--- src/migration/ember-engine/addon/route-stylesheets.js
+++ src/migration/ember-engine/addon/route-stylesheets.js
@@ -3,11 +3,23 @@ import glob from 'glob';
 import { mapPaths } from '../../../utils/map-paths.js';
 
 export function migrationStrategyForRouteStylesheets(projectRoot) {
-  const oldPaths = glob.sync('addon/!(components)/**/styles.css', {
+  const oldPaths = glob.sync('addon/!(components)/**/styles.{css,scss}', {
     cwd: projectRoot,
   });
 
   return oldPaths.map((oldPath) => {
+    if (oldPath.endsWith('.scss')) {
+      return mapPaths(oldPath, {
+        find: {
+          directory: 'addon',
+          file: 'styles.scss',
+        },
+        replace(key) {
+          return `addon/styles/${key}.scss`;
+        },
+      });
+    }
+
     return mapPaths(oldPath, {
       find: {
         directory: 'addon',
