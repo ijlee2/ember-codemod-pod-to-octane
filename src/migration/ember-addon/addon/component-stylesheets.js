63b2a2f67f440ebe30e7b48effa2f0cf324c4764
ijlee2 <ijlee2@users.noreply.github.com>
2022-07-25 14:40:49 +0200

    feature: Updated the migration strategy for component stylesheets

--- src/migration/ember-addon/addon/component-stylesheets.js
+++ src/migration/ember-addon/addon/component-stylesheets.js
@@ -3,11 +3,23 @@ import glob from 'glob';
 import { mapPaths } from '../../../utils/map-paths.js';
 
 export function migrationStrategyForComponentStylesheets(projectRoot) {
-  const oldPaths = glob.sync('addon/components/**/styles.css', {
+  const oldPaths = glob.sync('addon/components/**/styles.{css,scss}', {
     cwd: projectRoot,
   });
 
   return oldPaths.map((oldPath) => {
+    if (oldPath.endsWith('.scss')) {
+      return mapPaths(oldPath, {
+        find: {
+          directory: 'addon/components',
+          file: 'styles.scss',
+        },
+        replace(key) {
+          return `addon/components/${key}.scss`;
+        },
+      });
+    }
+
     return mapPaths(oldPath, {
       find: {
         directory: 'addon/components',
