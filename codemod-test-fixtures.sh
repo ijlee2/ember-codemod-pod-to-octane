0c625e607eccdca78607b646c1419933aabfc064
ijlee2 <ijlee2@users.noreply.github.com>
2023-02-14 12:56:14 +0100

    chore: Added sass fixtures to the codemod-test-fixtures script

--- codemod-test-fixtures.sh
+++ codemod-test-fixtures.sh
@@ -13,9 +13,12 @@
 #---------
 
 ./codemod-test-fixture.sh -t "addon" ember-addon/javascript
+./codemod-test-fixture.sh -t "addon" ember-addon/sass
 ./codemod-test-fixture.sh -t "addon" ember-addon/typescript
 ./codemod-test-fixture.sh -t "app" ember-app/javascript
 ./codemod-test-fixture.sh -t "app" -p "pods" ember-app/pod-path
+./codemod-test-fixture.sh -t "app" ember-app/sass
 ./codemod-test-fixture.sh -t "app" ember-app/typescript
 ./codemod-test-fixture.sh -t "engine" ember-engine/javascript
+./codemod-test-fixture.sh -t "engine" ember-engine/sass
 ./codemod-test-fixture.sh -t "engine" ember-engine/typescript
