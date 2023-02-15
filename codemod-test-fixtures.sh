30ca061ade9b845c268646fbd017e4c7b3413858
ijlee2 <ijlee2@users.noreply.github.com>
2023-02-15 14:37:15 +0100

    chore: Added test fixtures (ember-engine)

--- codemod-test-fixtures.sh
+++ codemod-test-fixtures.sh
@@ -19,6 +19,8 @@
 ./codemod-test-fixture.sh -t "app" -p "pods" ember-app/pod-path
 ./codemod-test-fixture.sh -t "app" ember-app/sass
 ./codemod-test-fixture.sh -t "app" ember-app/typescript
+./codemod-test-fixture.sh -t "engine" ember-engine/absolute-imports
 ./codemod-test-fixture.sh -t "engine" ember-engine/javascript
+./codemod-test-fixture.sh -t "engine" ember-engine/relative-imports
 ./codemod-test-fixture.sh -t "engine" ember-engine/sass
 ./codemod-test-fixture.sh -t "engine" ember-engine/typescript
