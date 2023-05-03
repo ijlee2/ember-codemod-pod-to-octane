94f16b4ccbf27895533f6e6ec62e27a8c29b53db
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-03 08:18:28 +0200

    chore: Updated file utilities

--- src/utils/files.js
+++ src/utils/files.js
@@ -1,3 +1,4 @@
+export * from './files/find-files.js';
 export * from './files/map-file-path.js';
 export * from './files/move-files.js';
 export * from './files/remove-directory-if-empty.js';
---
+++ src/utils/files/find-files.js
@@ -0,0 +1,28 @@
+import glob from 'glob';
+
+export function findFiles(pattern, { cwd, ignoreList = [] }) {
+  if (!pattern) {
+    throw new RangeError('ERROR: The glob pattern is unknown.\n');
+  }
+
+  if (!cwd) {
+    throw new RangeError('ERROR: The current working directory is unknown.\n');
+  }
+
+  const filePaths = glob.sync(pattern, {
+    cwd,
+    dot: true,
+    ignore: ignoreList,
+    nodir: true,
+  });
+
+  return filePaths;
+}
+
+export function unionize(files) {
+  if (files.length <= 1) {
+    return files.join(',');
+  }
+
+  return `{${files.join(',')}}`;
+}
