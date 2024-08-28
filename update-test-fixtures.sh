72e7f7155469610e86d49c4ae25899452817a812
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-28 10:13:53 +0200

    chore: Simplified script

---
+++ update-test-fixtures.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env sh
+
+#----------
+#
+#  A. Purpose
+#
+#    Fix all test fixtures after updating the source code.
+#
+#  B. Usage
+#
+#    ./update-test-fixtures.sh
+#
+#---------
+
+# Compile TypeScript
+pnpm build
+
+# Update fixtures
+rm -r "tests/fixtures/ember-addon/javascript/output"
+cp -r "tests/fixtures/ember-addon/javascript/input" "tests/fixtures/ember-addon/javascript/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-addon/javascript/output" \
+  --type addon
+
+# Update fixtures
+rm -r "tests/fixtures/ember-addon/sass/output"
+cp -r "tests/fixtures/ember-addon/sass/input" "tests/fixtures/ember-addon/sass/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-addon/sass/output" \
+  --type addon
+
+# Update fixtures
+rm -r "tests/fixtures/ember-addon/typescript/output"
+cp -r "tests/fixtures/ember-addon/typescript/input" "tests/fixtures/ember-addon/typescript/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-addon/typescript/output" \
+  --type addon
+
+# Update fixtures
+rm -r "tests/fixtures/ember-app/javascript/output"
+cp -r "tests/fixtures/ember-app/javascript/input" "tests/fixtures/ember-app/javascript/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-app/javascript/output" \
+  --type app
+
+# Update fixtures
+rm -r "tests/fixtures/ember-app/pod-path/output"
+cp -r "tests/fixtures/ember-app/pod-path/input" "tests/fixtures/ember-app/pod-path/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --pod-path pods \
+  --root "tests/fixtures/ember-app/pod-path/output" \
+  --type app
+
+# Update fixtures
+rm -r "tests/fixtures/ember-app/sass/output"
+cp -r "tests/fixtures/ember-app/sass/input" "tests/fixtures/ember-app/sass/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-app/sass/output" \
+  --type app
+
+# Update fixtures
+rm -r "tests/fixtures/ember-app/typescript/output"
+cp -r "tests/fixtures/ember-app/typescript/input" "tests/fixtures/ember-app/typescript/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-app/typescript/output" \
+  --type app
+
+# Update fixtures
+rm -r "tests/fixtures/ember-engine/absolute-imports/output"
+cp -r "tests/fixtures/ember-engine/absolute-imports/input" "tests/fixtures/ember-engine/absolute-imports/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-engine/absolute-imports/output" \
+  --type engine
+
+# Update fixtures
+rm -r "tests/fixtures/ember-engine/javascript/output"
+cp -r "tests/fixtures/ember-engine/javascript/input" "tests/fixtures/ember-engine/javascript/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-engine/javascript/output" \
+  --type engine
+
+# Update fixtures
+rm -r "tests/fixtures/ember-engine/relative-imports/output"
+cp -r "tests/fixtures/ember-engine/relative-imports/input" "tests/fixtures/ember-engine/relative-imports/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-engine/relative-imports/output" \
+  --type engine
+
+# Update fixtures
+rm -r "tests/fixtures/ember-engine/sass/output"
+cp -r "tests/fixtures/ember-engine/sass/input" "tests/fixtures/ember-engine/sass/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-engine/sass/output" \
+  --type engine
+
+# Update fixtures
+rm -r "tests/fixtures/ember-engine/typescript/output"
+cp -r "tests/fixtures/ember-engine/typescript/input" "tests/fixtures/ember-engine/typescript/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/ember-engine/typescript/output" \
+  --type engine
