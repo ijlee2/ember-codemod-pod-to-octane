2d1c7965084e346e4abf44fa76b79e48421774c5
ijlee2 <ijlee2@users.noreply.github.com>
2026-07-01 11:23:45 +0200

    refactor: Simplified methods (v1-addon, routes)

--- src/migration/v1-addon/steps/create-file-path-maps/addon/index.ts
+++ src/migration/v1-addon/steps/create-file-path-maps/addon/index.ts
@@ -1,18 +1,12 @@
 import type { FilePathMap, Options } from '../../../../../types/index.js';
 import { mapComponents } from './map-components.js';
-import { mapRouteControllers } from './map-route-controllers.js';
-import { mapRouteRoutes } from './map-route-routes.js';
-import { mapRouteStylesheets } from './map-route-stylesheets.js';
-import { mapRouteTemplates } from './map-route-templates.js';
+import { mapRoutes } from './map-routes.js';
 import { mapServices } from './map-services.js';
 
 export function mapAddonFolder(options: Options): FilePathMap {
   return new Map([
     ...mapComponents(options),
-    ...mapRouteControllers(options),
-    ...mapRouteRoutes(options),
-    ...mapRouteStylesheets(options),
-    ...mapRouteTemplates(options),
+    ...mapRoutes(options),
     ...mapServices(options),
   ]);
 }
--- src/migration/v1-addon/steps/create-file-path-maps/app/index.ts
+++ src/migration/v1-addon/steps/create-file-path-maps/app/index.ts
@@ -1,16 +1,12 @@
 import type { FilePathMap, Options } from '../../../../../types/index.js';
 import { mapComponents } from './map-components.js';
-import { mapRouteControllers } from './map-route-controllers.js';
-import { mapRouteRoutes } from './map-route-routes.js';
-import { mapRouteTemplates } from './map-route-templates.js';
+import { mapRoutes } from './map-routes.js';
 import { mapServices } from './map-services.js';
 
 export function mapAppFolder(options: Options): FilePathMap {
   return new Map([
     ...mapComponents(options),
-    ...mapRouteControllers(options),
-    ...mapRouteRoutes(options),
-    ...mapRouteTemplates(options),
+    ...mapRoutes(options),
     ...mapServices(options),
   ]);
 }
